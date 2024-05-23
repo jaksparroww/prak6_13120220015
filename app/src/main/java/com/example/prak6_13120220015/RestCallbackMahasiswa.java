73bf2c77ddfa675494789756cf164e1e9eb26107
unknown <saifuddinudin108@gmail.com>
2024-05-23 23:20:40 +0700

    Initial commit

---
+++ app/src/main/java/com/example/prak6_13120220015/RestCallbackMahasiswa.java
@@ -0,0 +1,7 @@
+package com.example.prak6_13120220015;
+
+import java.util.ArrayList;
+
+public interface RestCallbackMahasiswa {
+    void requestDataMhsSuccess(ArrayList<com.example.a1_prak6_13120220015.Mahasiswa> arrayList);
+}
